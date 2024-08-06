5e0753f35a1c6ee6ba20bf84f01ad2bfa676fac6
= <carl-neben@gmx.de>
2024-08-06 23:09:42 +0200

    Added lexer module

--- src/lexer/mod.rs
+++ src/lexer/mod.rs
@@ -1,1 +1,45 @@
+use crate::common::text::{location::Location, Source};
+
 pub mod token;
+
+pub struct Lexer {
+    source: Source,
+    position: Location,
+}
+
+impl Lexer {
+    pub fn new(source: Source) -> Self {
+        Lexer {
+            source: source.clone(),
+            position: Location::new(source, 0),
+        }
+    }
+
+    pub fn source(&self) -> Source {
+        self.source.clone()
+    }
+
+    pub fn position(&self) -> Location {
+        self.position.clone()
+    }
+
+    pub fn chars(&self) -> Vec<char> {
+        self.source.1.chars().collect()
+    }
+
+    pub fn peek(&self) -> Option<char> {
+        self.chars().get(self.position.index()).copied()
+    }
+
+    pub fn advance(&mut self) -> bool {
+        if self.position.is_eof() { return false; }
+        self.position.advance();
+        true
+    }
+
+    pub fn pop(&mut self) -> Option<char> {
+        let current = self.peek();
+        self.advance();
+        current
+    }
+}
