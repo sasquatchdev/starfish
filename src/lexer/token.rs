06a8e27b747c879b4ea1f05032e43e48a74f53c0
= <carl-neben@gmx.de>
2024-08-06 23:04:07 +0200

    Added lexer and lexer::token modules

---
+++ src/lexer/token.rs
@@ -0,0 +1,53 @@
+use crate::common::text::span::Span;
+
+#[derive(Debug, Clone)]
+pub struct Token {
+    span: Span,
+    lexeme: String,
+    kind: TokenKind,
+    flag: TokenFlag,
+}
+
+#[derive(Debug, Clone)]
+pub enum TokenKind {
+    Floating(f64),
+    Unsigned(u64),
+    Signed(i64),
+
+    Meta
+}
+
+#[derive(Debug, Clone)]
+pub enum TokenFlag {
+    None,
+    Info(String),
+    Warn(String),
+    Error(String),
+    Fatal(String),
+}
+
+impl Token {
+    pub fn new(span: Span, lexeme: String, kind: TokenKind, flag: TokenFlag) -> Self {
+        Token { span, lexeme, kind, flag }
+    }
+    
+    pub fn new_meta(span: Span, flag: TokenFlag) -> Self {
+        Token { span, flag, lexeme: String::new(), kind: TokenKind::Meta }
+    }
+
+    pub fn span(&self) -> Span {
+        self.span.clone()
+    }
+
+    pub fn lexeme(&self) -> String {
+        self.lexeme.clone()
+    }
+
+    pub fn kind(&self) -> TokenKind {
+        self.kind.clone()
+    }
+
+    pub fn flag(&self) -> TokenFlag {
+        self.flag.clone()
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused)]
 
 mod common;
+mod lexer;
 
 fn main() {
     println!("Hello, world!");
