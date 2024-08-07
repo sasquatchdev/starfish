62bdd09ac4349d2f9a399fb6073d27a9c2c274cf
= <carl-neben@gmx.de>
2024-08-07 18:26:24 +0200

    Changed parser::node module
    Added new, new_simple, new_meta and getter functions

--- src/parser/node.rs
+++ src/parser/node.rs
@@ -1,4 +1,4 @@
-use crate::common::{flag::Flag, text::span::Span};
+use crate::common::{flag::Flag, text::{location::Location, span::Span}};
 
 #[derive(Debug, Clone)]
 pub struct Node {
@@ -19,4 +19,38 @@ pub enum NodeKind {
         right: Box<Node>,
         operator: String,
     },
+
+    Meta
+}
+
+impl Node {
+    pub fn new(span: Span, flag: Flag, lexeme: String, kind: NodeKind) -> Self {
+        Self {
+            span,
+            flag,
+            lexeme,
+            kind,
+        }
+    }
+
+    pub fn new_simple(span: Span, lexeme: String, kind: NodeKind) -> Self {
+        Self::new(span, Flag::None, lexeme, kind)
+    }
+
+    pub fn new_meta(location: Location, flag: Flag) -> Self {
+        Self::new(Span::new_single(location), flag, String::new(), NodeKind::Meta)
+    }
+
+    pub fn span(&self) -> &Span {
+        &self.span
+    }
+    pub fn flag(&self) -> &Flag {
+        &self.flag
+    }
+    pub fn lexeme(&self) -> &str {
+        &self.lexeme
+    }
+    pub fn kind(&self) -> &NodeKind {
+        &self.kind
+    }
 }
